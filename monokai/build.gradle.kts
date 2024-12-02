6362a45ed51e5af112f0939e97ac19716f63d0f8
Václav Beneš <vaclav.benes@gendigital.com>
2024-12-02 13:12:40 +0100

    Updte kotlin, fleet-sdk, runtime versions

--- monokai/build.gradle.kts
+++ monokai/build.gradle.kts
@@ -3,15 +3,10 @@ plugins {
     alias(libs.plugins.fleet.plugin)
 }
 
-version = "0.3.0"
+version = "0.4.0"
 
 fleetPlugin {
-
-    // STEP 1: chose an ID for your plugin, it must be unique
-    //
      id = "monokai.basic.theme"
-
-    // STEP 2: set up the sensible metadata for your plugin
      metadata {
          readableName = "Monokai Basic"
          description = "Classic monokai theme from vscode"
